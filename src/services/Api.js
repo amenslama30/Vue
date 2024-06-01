53e9342b74df4bf548426d1710b54bbae90c01ad
amenslama30 <amenslama25@gmail.com>
2024-06-01 12:37:16 +0100

    updating

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: `https://localhost:8081/`,
+        baseURL: `/api`,
         withCredentials: true
     })
 }
